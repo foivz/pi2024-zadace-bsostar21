7366af52c11301e512e093c69cfd31f5a0d34e41
Borna Šoštar <bsostar21@foi.hr>
2024-06-02 10:24:58 +0200

    Ispravljeni nazivi gumba u formi za zaposlenike

--- Software/BŠoštar - GLines/FormaZaZaposlenike.Designer.cs
+++ Software/BŠoštar - GLines/FormaZaZaposlenike.Designer.cs
@@ -43,10 +43,10 @@
             this.label1.BackColor = System.Drawing.Color.DarkBlue;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(294, 29);
-            this.label1.Margin = new System.Windows.Forms.Padding(285, 20, 285, 200);
+            this.label1.Location = new System.Drawing.Point(220, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(214, 16, 214, 162);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(195, 54);
+            this.label1.Size = new System.Drawing.Size(157, 44);
             this.label1.TabIndex = 1;
             this.label1.Text = "GLINES";
             // 
@@ -55,9 +55,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(292, 102);
+            this.label2.Location = new System.Drawing.Point(219, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(199, 32);
+            this.label2.Size = new System.Drawing.Size(158, 26);
             this.label2.TabIndex = 4;
             this.label2.Text = "ZAPOSLENICI";
             // 
@@ -65,10 +66,10 @@
             // 
             this.gumbVozilo.BackColor = System.Drawing.Color.Black;
             this.gumbVozilo.ForeColor = System.Drawing.Color.Transparent;
-            this.gumbVozilo.Location = new System.Drawing.Point(29, 378);
-            this.gumbVozilo.Margin = new System.Windows.Forms.Padding(20);
+            this.gumbVozilo.Location = new System.Drawing.Point(22, 307);
+            this.gumbVozilo.Margin = new System.Windows.Forms.Padding(15, 16, 15, 16);
             this.gumbVozilo.Name = "gumbVozilo";
-            this.gumbVozilo.Size = new System.Drawing.Size(110, 51);
+            this.gumbVozilo.Size = new System.Drawing.Size(82, 41);
             this.gumbVozilo.TabIndex = 9;
             this.gumbVozilo.Text = "Vozilo";
             this.gumbVozilo.UseVisualStyleBackColor = false;
@@ -78,10 +79,10 @@
             // 
             this.gumbStanica.BackColor = System.Drawing.Color.Black;
             this.gumbStanica.ForeColor = System.Drawing.Color.Transparent;
-            this.gumbStanica.Location = new System.Drawing.Point(174, 378);
-            this.gumbStanica.Margin = new System.Windows.Forms.Padding(15);
+            this.gumbStanica.Location = new System.Drawing.Point(130, 307);
+            this.gumbStanica.Margin = new System.Windows.Forms.Padding(11, 12, 11, 12);
             this.gumbStanica.Name = "gumbStanica";
-            this.gumbStanica.Size = new System.Drawing.Size(110, 51);
+            this.gumbStanica.Size = new System.Drawing.Size(82, 41);
             this.gumbStanica.TabIndex = 10;
             this.gumbStanica.Text = "Stanica";
             this.gumbStanica.UseVisualStyleBackColor = false;
@@ -91,10 +92,10 @@
             // 
             this.gumbLinija.BackColor = System.Drawing.Color.Black;
             this.gumbLinija.ForeColor = System.Drawing.Color.Transparent;
-            this.gumbLinija.Location = new System.Drawing.Point(314, 378);
-            this.gumbLinija.Margin = new System.Windows.Forms.Padding(15);
+            this.gumbLinija.Location = new System.Drawing.Point(236, 307);
+            this.gumbLinija.Margin = new System.Windows.Forms.Padding(11, 12, 11, 12);
             this.gumbLinija.Name = "gumbLinija";
-            this.gumbLinija.Size = new System.Drawing.Size(110, 51);
+            this.gumbLinija.Size = new System.Drawing.Size(82, 41);
             this.gumbLinija.TabIndex = 11;
             this.gumbLinija.Text = "Linija";
             this.gumbLinija.UseVisualStyleBackColor = false;
@@ -104,10 +105,10 @@
             // 
             this.gumbVozniRed.BackColor = System.Drawing.Color.Black;
             this.gumbVozniRed.ForeColor = System.Drawing.Color.Transparent;
-            this.gumbVozniRed.Location = new System.Drawing.Point(454, 378);
-            this.gumbVozniRed.Margin = new System.Windows.Forms.Padding(15);
+            this.gumbVozniRed.Location = new System.Drawing.Point(340, 307);
+            this.gumbVozniRed.Margin = new System.Windows.Forms.Padding(11, 12, 11, 12);
             this.gumbVozniRed.Name = "gumbVozniRed";
-            this.gumbVozniRed.Size = new System.Drawing.Size(110, 51);
+            this.gumbVozniRed.Size = new System.Drawing.Size(82, 41);
             this.gumbVozniRed.TabIndex = 12;
             this.gumbVozniRed.Text = "Vozni red";
             this.gumbVozniRed.UseVisualStyleBackColor = false;
@@ -117,21 +118,21 @@
             // 
             this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.button5.ForeColor = System.Drawing.Color.Black;
-            this.button5.Location = new System.Drawing.Point(643, 378);
-            this.button5.Margin = new System.Windows.Forms.Padding(15, 15, 20, 15);
+            this.button5.Location = new System.Drawing.Point(482, 307);
+            this.button5.Margin = new System.Windows.Forms.Padding(11, 12, 15, 12);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(110, 51);
+            this.button5.Size = new System.Drawing.Size(82, 41);
             this.button5.TabIndex = 13;
-            this.button5.Text = "Natrag";
+            this.button5.Text = "Odjava";
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // FormaZaZaposlenike
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cyan;
-            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.ClientSize = new System.Drawing.Size(586, 368);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.gumbVozniRed);
             this.Controls.Add(this.gumbLinija);
@@ -140,6 +141,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormaZaZaposlenike";
             this.Text = "FormaZaZaposlenike";
             this.Load += new System.EventHandler(this.FormaZaZaposlenike_Load);
